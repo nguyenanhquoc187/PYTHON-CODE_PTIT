b4ab12da455705c2997bdc6c12eea334de4d4134
Nguyễn Anh Quốc <nguyenanhquoc187@gmail.com>
2022-12-12 21:58:26 +0700

    upload

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
-d = []
+from math import comb
+
+n = int(input())
+a = []
+for i in range(n):
+    b = list(input())
+    a.append(b)
+soCapdongxu = 0
+for i in a:
+    dem = i.count('C')
+    if dem >=2: soCapdongxu+= comb(dem,2)
+for i in range(n):
+    dem = 0
+    for j in range(n):
+        if a[j][i] == 'C': dem+=1
+    if dem>=2: soCapdongxu+= comb(dem,2)
+print(soCapdongxu)
--- ĐẾM CẶP ĐỒNG XU.py
+++ ĐẾM CẶP ĐỒNG XU.py
@@ -1,8 +1,5 @@
-from math import factorial
+from math import comb
 
-
-def Ckn(n , k):
-    return factorial(n)//(factorial(k)*factorial(n-k))
 n = int(input())
 a = []
 for i in range(n):
@@ -11,10 +8,10 @@ for i in range(n):
 soCapdongxu = 0
 for i in a:
     dem = i.count('C')
-    if dem >=2: soCapdongxu+= Ckn(dem,2)
+    if dem >=2: soCapdongxu+= comb(dem,2)
 for i in range(n):
     dem = 0
     for j in range(n):
         if a[j][i] == 'C': dem+=1
-    if dem>=2: soCapdongxu+= Ckn(dem,2)
+    if dem>=2: soCapdongxu+= comb(dem,2)
 print(soCapdongxu)
